25a07a62ca71ab273cdc94c3975c366d60c19548
Dishank Tak <39084848+Dishank99@users.noreply.github.com>
2021-02-08 20:31:05 +0530

    Find the Union and Intersection of the two sorted arrays.

--- Arrays/5.py
+++ Arrays/5.py
@@ -2,6 +2,28 @@
 
 # approach 1 is converting both array to sets and find union and intersection => tc = n, sc = n approach
 
+def find_intersection(array1, array2):
+
+    # base cases
+    if not array1 or not array2:
+        return []
+
+    i = j = 0
+
+    intersection_array = []
+
+    while i < len(array1) and j < len(array2):
+        if array1[i] < array2[j]: # since arrays are sorted to move towards finding common ele
+            i+=1
+        elif array2[j] < array1[i]:
+            j+=1
+        else: # if common ele is found
+            intersection_array.append(array1[i])
+            i+=1
+            j+=1
+
+    return intersection_array
+
 def find_union(array1, array2):
 
     # base cases
@@ -18,20 +40,32 @@ def find_union(array1, array2):
 
     while i < len(array1) and j < len(array2):
         if array1[i] < array2[j]: # since arrays are sorted to move towards finding common ele
+            union_array.append(array1[i])
             i+=1
         elif array2[j] < array1[i]:
+            union_array.append(array2[j])
             j+=1
         else: # if common ele is found
             union_array.append(array1[i])
             i+=1
             j+=1
+    
+    while i < len(array1):
+        union_array.append(array1[i])
+        i+=1
+    
+    while j < len(array2):
+        union_array.append(array2[j])
+        j+=1
 
     return union_array
+        
 
 if __name__ == '__main__':
-    array1 = [85, 25, 1, 32, 54, 6]
-    array2 = [85, 2]
+    array1 = [1, 2, 3, 4, 5, 6]
+    array2 = [1, 2, 3]
 
+    intersection = find_intersection(array1, array2)
     union = find_union(array1, array2)
 
     print(union)
